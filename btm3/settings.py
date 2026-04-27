41c43ea058501e859eebfd914d097c79061e849c
davil0136 <anilmaikap0136@gmail.com>
2026-04-27 21:38:43 +0530

    Fix middleware error

--- btm3/settings.py
+++ btm3/settings.py
@@ -72,7 +72,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django_auto_logout.middleware.auto_logout'
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 # password hashers
